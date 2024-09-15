5be2a5e1b70af7efa0611dd86c2c0a9d5d44dddd
Rishu <vishwakarmarishabh366@gmail.com>
2024-09-15 06:48:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RishuxCoder/StrangerRishu",
+    "https://github.com/RishuBot/RishuMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

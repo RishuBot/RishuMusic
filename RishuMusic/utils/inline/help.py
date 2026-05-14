3190200dc04c06409d62c7562cdfdcf8213e9218
Rishu <vishwakarmarishabh366@gmail.com>
2026-05-14 12:58:40 +0530

    Update help.py

--- RishuMusic/utils/inline/help.py
+++ RishuMusic/utils/inline/help.py
@@ -14,68 +14,59 @@ def help_pannel(_, is_sudo, START: Union[bool, int] = None):
         ),
     ]
     mark = second if START else first
+
     upl = [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-        ]
-    
- ##   if is_sudo:
-   ##     upl.append(
-        #    [
-             #   InlineKeyboardButton(
-               #     text="Ai/TTS/IMAGE Settings",#
-                    #callback_data="help_callback hb16",
-           #     )
-          #  ]
-        )
-    
- #   upl.append(mark)
+        [
+            InlineKeyboardButton(
+                text=_["H_B_10"],
+                callback_data="help_callback hb10",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_1"],
+                callback_data="help_callback hb1",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_2"],
+                callback_data="help_callback hb2",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_3"],
+                callback_data="help_callback hb3",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_4"],
+                callback_data="help_callback hb4",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_5"],
+                callback_data="help_callback hb5",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_6"],
+                callback_data="help_callback hb6",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_7"],
+                callback_data="help_callback hb7",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_8"],
+                callback_data="help_callback hb8",
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_9"],
+                callback_data="help_callback hb9",
+            ),
+        ],
+    ]
+
+    upl.append(mark)
 
     return InlineKeyboardMarkup(upl)
 

532e05c66748daa2b8a296ca8ab42a9b70caaab7
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-05-14 11:12:07 +0530

    Update start.py

--- RishuMusic/plugins/bot/start.py
+++ RishuMusic/plugins/bot/start.py
@@ -1,162 +1,90 @@
-import asyncio
 import random
 import time
+
+from py_yt import VideosSearch
 from pyrogram import filters
 from pyrogram.enums import ChatType
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
-from youtubesearchpython.__future__ import VideosSearch
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 import config
 from RishuMusic import app
 from RishuMusic.misc import _boot_
 from RishuMusic.plugins.sudo.sudoers import sudoers_list
-from RishuMusic.utils import bot_sys_stats
-from RishuMusic.utils.database import (
-    add_served_chat,
-    add_served_user,
-    blacklisted_chats,
-    get_lang,
-    get_served_chats,
-    get_served_users,
-    is_banned_user,
-    is_on_off,
-)
+from RishuMusic.utils.database import (add_served_chat, add_served_user,
+                                       blacklisted_chats, get_lang,
+                                       is_banned_user, is_on_off)
 from RishuMusic.utils.decorators.language import LanguageStart
 from RishuMusic.utils.formatters import get_readable_time
 from RishuMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-NEXIO_VD = [
-    "https://telegra.ph/file/89c5023101b65f21fb401.mp4",
-    "https://telegra.ph/file/bbc914cce6cce7f607641.mp4",
-    "https://telegra.ph/file/abc578ecc222d28a861ba.mp4",
-    "https://telegra.ph/file/065f40352707e9b5b7c15.mp4",
-    "https://telegra.ph/file/52ceaf02eae7eed6c9fff.mp4",
-    "https://telegra.ph/file/299108f6ac08f4e65e47a.mp4",
-    "https://telegra.ph/file/7a4e08bd04d628de71fc1.mp4",
-    "https://telegra.ph/file/0ad8b932fe5f7684f941c.mp4",
-    "https://telegra.ph/file/95ebe2065cfb1ac324a1c.mp4",
-    "https://telegra.ph/file/98cf22ccb987f9fedac5e.mp4",
-    "https://telegra.ph/file/f1b1754fc9d01998f24df.mp4",
-    "https://telegra.ph/file/421ee22ed492a7b8ce101.mp4",
+EFFECT_ID = [
+    5046589136895476101,
+    5046589136895476101,
+    5046589136895476101,
+    5046589136895476101,
 ]
 
-HIMANSHI = [
-    "https://files.catbox.moe/jrupn9.jpg",
-    "https://files.catbox.moe/5z141p.jpg",
-    "https://files.catbox.moe/fnl0h7.jpg",
-    "https://files.catbox.moe/1lz1go.jpg",
-    "https://files.catbox.moe/avackl.jpg",
-    "https://files.catbox.moe/1yrzwz.jpg",
-    "https://files.catbox.moe/6y22qw.jpg",
-    "https://files.catbox.moe/gnnsf2.jpg",
-    "https://files.catbox.moe/ss6r60.jpg",
-    "https://files.catbox.moe/yuob18.jpg",
-    "https://files.catbox.moe/i9xrrp.jpg",
-    "https://files.catbox.moe/a9tx8f.jpg",
-    "https://files.catbox.moe/wlt26x.jpg",
-    "https://files.catbox.moe/c1lylh.jpg",
-    "https://files.catbox.moe/82eymp.jpg",
-]
-
-STICKERS = [
-    "CAACAgEAAxkBAAEOAtpnzB8aRVyieCZ0WNkygpbbtI3_YQAC-gUAAjIJwUUrDoSRTa122zYE",
-    "CAACAgEAAxkBAAEOAthnzB8Y3MVOmvy_Oh3D8iby8V-19AAClQQAAhsLyEXZmJfqSdnWiDYE",
-    "CAACAgEAAxkBAAEOAtZnzB8Uesx6GApr2AlCNLZmQCiETgACswQAAozgwUX2FBzX8QQMmzYE",
-    "CAACAgEAAxkBAAEOAt1nzB8eR9Q5HAy3WsC9JWY3QFPFkAACpQQAAry3yUVbTjRNiKwMWTYE",
-    "CAACAgEAAxkBAAEOAtxnzB8dQSDNaSqlv7JzTvH0hyvM8AAC8gQAAsKBwUWj9bD__o2TqDYE",
+Kanha_Pic = [
+    "https://i.ibb.co/VYvJMJgh/x.jpg",
+    "https://i.ibb.co/LXGxYRnJ/x.jpg",
+    "https://i.ibb.co/YTPV4Swv/x.jpg",
+    "https://i.ibb.co/CKsQzVHT/x.jpg",
+    "https://i.ibb.co/4n7Fcz75/x.jpg",
+    "https://i.ibb.co/qMCt4gQm/x.jpg",
+    "https://i.ibb.co/pjvF9W5B/x.jpg",
+    "https://i.ibb.co/WXs66zC/x.jpg",
+    "https://i.ibb.co/W4PSwTs0/x.jpg",
+    "https://i.ibb.co/Kc8fY1B9/x.jpg",
+    "https://i.ibb.co/84DkKPSy/x.jpg",
+    "https://i.ibb.co/nsVQcYkL/x.jpg",
+    "https://i.ibb.co/tnDc1hd/x.jpg"
 ]
 
 
-async def send_sticker(message: Message):
-    await message.reply_sticker(random.choice(STICKERS))
-
-
-async def progress_bar(message: Message):
-    # Pehle sticker bhejo
-    sticker_msg = await message.reply_sticker(random.choice(STICKERS))
-
-    # 1 second wait karke sticker delete karo
-    await asyncio.sleep(1)
-    await sticker_msg.delete()
-
-    # Progress bar start karo
-    baby = await message.reply_text("[□□□□□□□□□□] 0%")
-
-    progress = [
-        "[■□□□□□□□□□] 10%",
-        "[■■□□□□□□□□] 20%",
-        "[■■■□□□□□□□] 30%",
-        "[■■■■□□□□□□] 40%",
-        "[■■■■■□□□□□] 50%",
-        "[■■■■■■□□□□] 60%",
-        "[■■■■■■■□□□] 70%",
-        "[■■■■■■■■□□] 80%",
-        "[■■■■■■■■■□] 90%",
-        "[■■■■■■■■■■] 100%",
-    ]
-
-    for step in progress:
-        await baby.edit_text(f"<b>{step}</b>")
-        await asyncio.sleep(0.3)  # Adjust delay for smooth updates
-
-    # Final message bhejo
-    await baby.edit_text("<b>❖ Jᴀʏ sʜʀᴇᴇ ʀᴀᴍ 🚩...</b>")
-    await asyncio.sleep(1)
-    await baby.delete()
+def get_start_img() -> str:
+    """Returns a random start image URL."""
+    return random.choice(Kanha_Pic)
 
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-
+    await message.react("🍓", big=True)
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-
-        if name.startswith("help"):
+        if name[0:4] == "help":
             keyboard = help_pannel(_)
-            # help uses video — leave as video (no spoiler for video)
-            await message.reply_video(
-                random.choice(NEXIO_VD),
+            return await message.reply_photo(
+                photo=get_start_img(),
+                has_spoiler=False,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
-            ),
-
-        elif name.startswith("sud"):
+            )
+        if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
-                await app.send_message(
+                return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=(
-                        f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n"
-                        f"<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n"
-                        f"<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}"
-                    ),
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-
-        elif name.startswith("inf"):
-            m = await message.reply_text("🔎")
-            query = str(name).replace("info_", "", 1)
+            return
+        if name[0:3] == "inf":
+            m = await message.reply_text("👀")
+            query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
-
-            title = duration = views = thumbnail = channellink = channel = link = published = None
             for result in (await results.next())["result"]:
-                title = result.get("title")
-                duration = result.get("duration")
-                views = result.get("viewCount", {}).get("short")
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
                 thumbnail = result["thumbnails"][0]["url"].split("?")[0]
                 channellink = result["channel"]["link"]
                 channel = result["channel"]["name"]
-                link = result.get("link")
-                published = result.get("publishedTime")
-
+                link = result["link"]
+                published = result["publishedTime"]
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
@@ -168,61 +96,32 @@ async def start_pm(client, message: Message, _):
                     ],
                 ]
             )
-
             await m.delete()
-            # thumbnail as a PHOTO spoiler (so user must tap to reveal)
             await app.send_photo(
                 chat_id=message.chat.id,
                 photo=thumbnail,
+                has_spoiler=True,
                 caption=searched_text,
                 reply_markup=key,
-                has_spoiler=True,
             )
-
             if await is_on_off(2):
-                await app.send_message(
+                return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=(
-                        f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n"
-                        f"<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n"
-                        f"<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}"
-                    ),
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-
     else:
         out = private_panel(_)
-        served_chats = len(await get_served_chats())
-        served_users = len(await get_served_users())
-        UP, CPU, RAM, DISK = await bot_sys_stats()
-
-        # Progress bar function call karo
-        await progress_bar(message)
-
-        # Video send karo (no spoiler needed for video)
-        await message.reply_video(
-            random.choice(NEXIO_VD),
-            caption=_["start_2"].format(
-                message.from_user.mention,
-                app.mention,
-                UP,
-                DISK,
-                CPU,
-                RAM,
-                served_users,
-                served_chats,
-            ),
+        await message.reply_photo(
+            photo=get_start_img(),
+            has_spoiler=True,
+            message_effect_id=random.choice(EFFECT_ID),
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
-has_spoiler=True,
         )
-
         if await is_on_off(2):
-            await app.send_message(
+            return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=(
-                    f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n"
-                    f"<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n"
-                    f"<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}"
-                ),
+                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
 
 
@@ -231,11 +130,11 @@ has_spoiler=True,
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_video(
-        random.choice(NEXIO_VD),
+    await message.reply_photo(
+        photo=get_start_img(),
+        has_spoiler=True,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
-has_spoiler=True,
     )
     return await add_served_chat(message.chat.id)
 
@@ -246,18 +145,15 @@ async def welcome(client, message: Message):
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
-                except Exception:
+                except:
                     pass
-
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
-
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
@@ -270,17 +166,16 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                # welcome photo as spoiler
                 await message.reply_photo(
-                    random.choice(HIMANSHI),
+                    photo=get_start_img(),
+                    has_spoiler=True,
                     caption=_["start_3"].format(
-                        message.from_user.mention,
+                        message.from_user.first_name,
                         app.mention,
                         message.chat.title,
                         app.mention,
                     ),
                     reply_markup=InlineKeyboardMarkup(out),
-                    has_spoiler=True,
                 )
                 await add_served_chat(message.chat.id)
                 await message.stop_propagation()

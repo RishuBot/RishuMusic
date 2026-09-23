dce959d24ac65a40a37d006a1c3e98e9d1e261a2
Rishu <vishwakarmarishabh366@gmail.com>
2026-09-23 16:46:32 +0530

    Update resume.py

--- RishuMusic/plugins/admins/resume.py
+++ RishuMusic/plugins/admins/resume.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from RishuMusic import app
-from RishuMusic.core.call import Anony
+from RishuMusic.core.call import Rishu
 from RishuMusic.utils.database import is_music_playing, music_on
 from RishuMusic.utils.decorators import AdminRightsCheck
 from RishuMusic.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Anony.resume_stream(chat_id)
+    await Rishu.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )

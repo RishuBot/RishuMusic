4596bd6763ccbbfef3df388bcc2fe322f3b1eed0
Rishu <vishwakarmarishabh366@gmail.com>
2026-09-23 16:45:59 +0530

    Update seek.py

--- RishuMusic/plugins/admins/seek.py
+++ RishuMusic/plugins/admins/seek.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from RishuMusic import YouTube, app
-from RishuMusic.core.call import Anony
+from RishuMusic.core.call import Rishu
 from RishuMusic.misc import db
 from RishuMusic.utils import AdminRightsCheck, seconds_to_min
 from RishuMusic.utils.inline import close_markup
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await Anony.seek_stream(
+        await Rishu.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),

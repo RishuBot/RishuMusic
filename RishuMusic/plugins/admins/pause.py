f7e3ae419f7f27cb9d8ac6123811c7b22af400f5
Rishu <vishwakarmarishabh366@gmail.com>
2026-09-23 16:48:27 +0530

    Update pause.py

--- RishuMusic/plugins/admins/pause.py
+++ RishuMusic/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from RishuMusic import app
-from RishuMusic.core.call import Anony
+from RishuMusic.core.call import Rishu
 from RishuMusic.utils.database import is_music_playing, music_off
 from RishuMusic.utils.decorators import AdminRightsCheck
 from RishuMusic.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anony.pause_stream(chat_id)
+    await Rishu.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )

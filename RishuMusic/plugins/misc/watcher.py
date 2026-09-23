13d9b12088312d470c6926b86dff3509db40c6db
Rishu <vishwakarmarishabh366@gmail.com>
2026-09-23 16:43:37 +0530

    Update watcher.py

--- RishuMusic/plugins/misc/watcher.py
+++ RishuMusic/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from RishuMusic import app
-from RishuMusic.core.call import Anony
+from RishuMusic.core.call import Rishu
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Anony.stop_stream_force(message.chat.id)
+    await Rishu.stop_stream_force(message.chat.id)

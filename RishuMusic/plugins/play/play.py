5406a94adf2c8614e0791efc65d04a70db00f51e
Rishu <vishwakarmarishabh366@gmail.com>
2026-09-23 16:38:23 +0530

    Update play.py

--- RishuMusic/plugins/play/play.py
+++ RishuMusic/plugins/play/play.py
@@ -289,7 +289,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Anony.stream_call(url)
+                await Rishu.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
